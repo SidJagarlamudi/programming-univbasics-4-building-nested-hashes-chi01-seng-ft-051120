722186242351803cec78bd7b4c9133007a8fe6b7
Sid Jagarlamudi <jaglings@gmail.com>
2020-04-15 23:16:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,9 @@ def monopoly_with_fourth_tier
   # complexity.
 monopoly ={
   
-  :railroads => 
+  :railroads => {
   
-  {:pieces => 4, 
+:pieces => 4, 
   :rent_in_dollars => {:one_piece_owned => 25,
   :two_pieces_owned =>50,
   :three_pieces_owned => 100,
